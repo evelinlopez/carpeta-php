caa294e1e26d608dd62632e486f21f31bf235a26
evelinlopez <54640557+evelinlopez@users.noreply.github.com>
2019-08-28 15:26:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//para realizar un comentario en php utilizamos //
+/*
+para realizar un comentario
+de mas de una linea
+utilizamos barra aterisco, aterisco barra 
+
+
+*/
+/*php es el acronimo de HyperText pre-processor o pre procesador de iper texto el cual se ejecuta en un servidor wep, que durante nuestra practica estara en nuestra computadora y cuando lo subamos a internet estara en el lugar en donde alojaremos nuetro sitio, en nuesttro caso 000webhost. esta integrado con html por lo que podemos utilizar muchas etiquetas del mismo. */
+$Result=0;
+$Nombre="Evelin";
+$Apellido="Lopez";
+echo $Nombre." ".$Apellido;
+$num1= 55 ; 
+$num2= 70 ;
+$Result= $num1 + $num2;
+echo $Result;
+
+?>
